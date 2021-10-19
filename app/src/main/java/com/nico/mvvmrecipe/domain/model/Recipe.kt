5e5da8ba74aa38b4d24e59f98dd8fd43feae70b8
Nico <johnnicomolon@yahoo.com>
2021-10-18 20:30:39 -0400

    Add title and cookingInstruction

--- app/src/main/java/com/nico/mvvmrecipe/domain/model/Recipe.kt
+++ app/src/main/java/com/nico/mvvmrecipe/domain/model/Recipe.kt
@@ -7,11 +7,12 @@ import kotlinx.parcelize.Parcelize
 data class Recipe(
     val id: Int? = null,
     val title: String? = null,
-    val publisher: String? = null,
     val featuredImage: String? = null,
     val rating: Int? = null,
+    val publisher: String? = null,
     val sourceUrl: String? = null,
     val description: String? = null,
+    val cookingInstruction: String? = null,
     val ingredients: List<String> = listOf(),
     val dateAdded: String? = null,
     val dateUpdated: String? = null
