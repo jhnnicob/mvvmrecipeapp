4feb1c28fe767367c4e0c2d5b4049f35ad5e750a
Nico <johnnicomolon@yahoo.com>
2021-10-19 01:11:34 -0400

    Add search textfield and ScrollableTabRow for chips

--- app/src/main/java/com/nico/mvvmrecipe/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/nico/mvvmrecipe/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,26 +1,29 @@
 package com.nico.mvvmrecipe.presentation.ui.recipe_list
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
-import com.nico.mvvmrecipe.R
+import com.nico.mvvmrecipe.presentation.components.FoodCategoryChips
 import com.nico.mvvmrecipe.presentation.components.RecipeCard
-import com.nico.mvvmrecipe.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -39,13 +42,81 @@ class RecipeListFragment: Fragment() {
 
                 val recipes = viewModel.recipes.value
 
-                LazyColumn{
-                    itemsIndexed(
-                        items = recipes
-                    ) { index, recipe ->
-                        RecipeCard(recipe = recipe, onClick = {})
+                val query = viewModel.query.value
+
+                val keyboardController = LocalFocusManager.current
+
+                Column {
+
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        color = Color.White,
+                        elevation = 8.dp
+                    ) {
+
+                        Column {
+                            Row(
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+
+                                TextField(
+                                    modifier = Modifier
+                                        .fillMaxWidth(0.9f)
+                                        .padding(8.dp),
+                                    value = query,
+                                    onValueChange = { newValue ->
+                                        viewModel.onQueryChanged(newValue)
+                                    },
+                                    label = {
+                                        Text(text = "Search")
+                                    },
+                                    keyboardOptions = KeyboardOptions(
+                                        keyboardType = KeyboardType.Text,
+                                        imeAction = ImeAction.Search
+                                    ),
+                                    leadingIcon = {
+                                        Icon(Icons.Filled.Search, "")
+                                    },
+                                    keyboardActions = KeyboardActions(
+                                        onSearch = {
+                                            viewModel.newSearch(query)
+                                            keyboardController.clearFocus()
+                                        }
+                                    ),
+                                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                                    colors = TextFieldDefaults.textFieldColors(
+                                        backgroundColor = MaterialTheme.colors.surface
+                                    )
+                                )
+                            }
+
+                                ScrollableTabRow(selectedTabIndex = 0, modifier = Modifier.fillMaxWidth()) {
+                                    for (category in getAllFoodCategories()) {
+                                        FoodCategoryChips(
+                                            category = category.value,
+                                            onExecuteSearch = {
+                                                viewModel.onQueryChanged(it)
+                                                viewModel.newSearch(it)
+                                            }
+                                        )
+                                    }
+                                }
+
+                        }
+
                     }
+
+                    LazyColumn{
+                        itemsIndexed(
+                            items = recipes
+                        ) { index, recipe ->
+                            RecipeCard(recipe = recipe, onClick = {})
+                        }
+                    }
+
                 }
+
             }
         }
     }
