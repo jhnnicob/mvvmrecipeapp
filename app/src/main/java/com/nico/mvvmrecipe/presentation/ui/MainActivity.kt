fad06a5394498264ae71f1eac3026033431fd783
Nico <johnnicomolon@yahoo.com>
2021-10-18 20:28:49 -0400

    remove unused codes

--- app/src/main/java/com/nico/mvvmrecipe/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/nico/mvvmrecipe/presentation/ui/MainActivity.kt
@@ -1,13 +1,18 @@
-package com.nico.mvvmrecipe
+package com.nico.mvvmrecipe.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.ui.core.setContent
+import com.nico.mvvmrecipe.R
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
