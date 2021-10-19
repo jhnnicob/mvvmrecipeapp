5bddc5ba5b50b80df3a4b775a4b0cb4feb4f62c9
Nico <johnnicomolon@yahoo.com>
2021-10-18 20:37:59 -0400

    Add RecipeRepository.kt

---
+++ app/src/main/java/com/nico/mvvmrecipe/repository/RecipeRepository.kt
@@ -0,0 +1,11 @@
+package com.nico.mvvmrecipe.repository
+
+import com.nico.mvvmrecipe.domain.model.Recipe
+
+interface RecipeRepository {
+
+    suspend fun search(token: String, page: Int, query: String): List<Recipe>
+
+    suspend fun get(token: String, id: Int): Recipe
+
+}
