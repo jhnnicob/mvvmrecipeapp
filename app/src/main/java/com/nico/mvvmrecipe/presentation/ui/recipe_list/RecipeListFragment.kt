d76404f0cd76ac51d5800e4685449cc15c06ec49
Nico <johnnicomolon@yahoo.com>
2021-10-18 20:37:16 -0400

    moved from mvvmrecipe,  add @AndroidEntryPoint and implement viewmodels into oncreateview

--- app/src/main/java/com/nico/mvvmrecipe/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/nico/mvvmrecipe/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,43 +1,51 @@
-package com.nico.mvvmrecipe
+package com.nico.mvvmrecipe.presentation.ui.recipe_list
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
-import androidx.ui.core.TextUnitType
+import com.nico.mvvmrecipe.R
+import com.nico.mvvmrecipe.presentation.components.RecipeCard
+import com.nico.mvvmrecipe.util.TAG
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RecipeListFragment: Fragment() {
+
+    private val viewModel: RecipeListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         return ComposeView(requireContext()).apply {
             setContent {
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = "Recipe List"
-                    )
-                    Button(
-                        onClick = { 
-                            findNavController().navigate(R.id.goToRecipe)
-                        }) {
-                        Text(text = "TO RECIPE FRAGMENT")
+
+                val recipes = viewModel.recipes.value
+
+                LazyColumn{
+                    itemsIndexed(
+                        items = recipes
+                    ) { index, recipe ->
+                        RecipeCard(recipe = recipe, onClick = {})
                     }
                 }
-
             }
         }
     }
